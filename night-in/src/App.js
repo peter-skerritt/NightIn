4eed3f42257197da869cbc8a2ea01e3366d09a37
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-11-07 20:42:46 +0000

    Basic styling added to the Homepage. Button functionality not yet added.

--- night-in/src/App.js
+++ night-in/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          NightIn
+    <div className="main">
+      <header className="main-header">
+          NightIn.
       </header>
     </div>
   );
