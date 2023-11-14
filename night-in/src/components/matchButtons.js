15ccb27c9d94220dfcd1ae73e267c0799e64bb8e
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-11-14 19:37:08 +0000

    Fixed git issues.

--- night-in/src/components/matchButtons.js
+++ night-in/src/components/matchButtons.js
@@ -4,7 +4,9 @@ import React from "react";
 function SideButton(){
     return(
     <div className="button-container">
-            <button className="side-button"></button>
+            <button className="side-button">
+                <p>ladddd</p>
+            </button>
             <button className="match-button">let's watch.</button>
 
         </div>
