d11a717b109ed44ff7fcba605c94992df292bc42
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-11-16 19:37:24 +0000

    Added small styling elements and fixed page div problems.

--- night-in/src/components/matchButtons.js
+++ night-in/src/components/matchButtons.js
@@ -2,15 +2,17 @@ import React from "react";
 import '../styles/matchButtons.css';
 
 
-function SideButton(){
-    return(
-    <div className="button-container">
+function SideButton() {
+    return (
+        <div className="button-container">
             <button className="side-button">
-                <p>Add Profile</p>
+                <span className="material-symbols-outlined" style={{ fontSize: '100pt' }}>
+                    person_add
+                </span>
             </button>
             <button className="match-button">let's watch.</button>
-
         </div>
-)}
+    );
+}
 
 export default SideButton
